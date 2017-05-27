a779aa6c64b02c107e5c9fa9ddac56b74e120a47
Michael Zingale <michael.zingale@stonybrook.edu>
2017-05-26 20:15:58 -0400

    remove some parameters

--- Source/Src_nd/meth_params.F90
+++ Source/Src_nd/meth_params.F90
@@ -29,12 +29,24 @@ module meth_params_module
   integer, save :: QTHERM, QVAR
   integer, save :: QRHO, QU, QV, QW, QPRES, QREINT, QTEMP, QGAME
   integer, save :: NQAUX, QGAMC, QC, QCSML, QDPDR, QDPDE
+#ifdef RADIATION
+  integer, save :: QGAMCG, QCG, QLAMS
+#endif
   integer, save :: QFA, QFS, QFX
 
   integer, save :: nadv
 
   ! NQ will be the total number of primitive variables, hydro + radiation
   integer, save :: NQ         
+
+#ifdef RADIATION
+  integer, save :: QRADVAR, QRAD, QRADHI, QPTOT, QREITOT
+  integer, save :: fspace_type
+  logical, save :: do_inelastic_scattering
+  logical, save :: comoving
+
+  real(rt)        , save :: flatten_pp_threshold = -1.e0_rt
+#endif
 
   integer, save :: npassive
   integer, save, allocatable :: qpass_map(:), upass_map(:)
@@ -43,6 +55,10 @@ module meth_params_module
   ! Note that the velocity indices here are picked to be the same value
   ! as in the primitive variable array
   integer, save :: NGDNV, GDRHO, GDU, GDV, GDW, GDPRES, GDGAME
+#ifdef RADIATION
+  integer, save :: GDLAMS, GDERADS
+#endif
+
   integer         , save :: numpts_1d
 
   real(rt)        , save, allocatable :: outflow_data_old(:,:)
@@ -72,6 +88,11 @@ module meth_params_module
   !$acc create(QRHO, QU, QV, QW, QPRES, QREINT, QTEMP) &
   !$acc create(QGAMC, QGAME) &
   !$acc create(NQ) &
+#ifdef RADIATION
+  !$acc create(QGAMCG, QCG, QLAMS) &
+  !$acc create(QRADVAR, QRAD, QRADHI, QPTOT, QREITOT) &
+  !$acc create(fspace_type, do_inelastic_scattering, comoving) &
+#endif
   !$acc create(QFA, QFS, QFX) &
   !$acc create(xl_ext, yl_ext, zl_ext, xr_ext, yr_ext, zr_ext)
 
@@ -123,36 +144,9 @@ module meth_params_module
   integer         , save :: hse_interp_temp
   integer         , save :: hse_reflect_vels
   real(rt), save :: cfl
-  real(rt), save :: dtnuc_e
-  real(rt), save :: dtnuc_X
-  integer         , save :: dtnuc_mode
-  real(rt), save :: dxnuc
-  integer         , save :: do_react
-  real(rt), save :: react_T_min
-  real(rt), save :: react_T_max
-  real(rt), save :: react_rho_min
-  real(rt), save :: react_rho_max
-  integer         , save :: disable_shock_burning
-  real(rt), save :: diffuse_cutoff_density
-  integer         , save :: do_grav
-  integer         , save :: grav_source_type
-  integer         , save :: do_rotation
-  real(rt), save :: rot_period
-  real(rt), save :: rot_period_dot
-  integer         , save :: rotation_include_centrifugal
-  integer         , save :: rotation_include_coriolis
-  integer         , save :: rotation_include_domegadt
-  integer         , save :: state_in_rotating_frame
-  integer         , save :: rot_source_type
-  integer         , save :: implicit_rotation_update
-  integer         , save :: rot_axis
-  real(rt), save :: point_mass
-  integer         , save :: point_mass_fix_solution
   integer         , save :: do_acc
   integer         , save :: grown_factor
   integer         , save :: track_grid_losses
-  real(rt), save :: const_grav
-  integer         , save :: get_g_from_phi
 
   !$acc declare &
   !$acc create(difmag, small_dens, small_temp) &
@@ -168,17 +162,8 @@ module meth_params_module
   !$acc create(density_reset_method, allow_negative_energy, allow_small_energy) &
   !$acc create(do_sponge, sponge_implicit, first_order_hydro) &
   !$acc create(hse_zero_vels, hse_interp_temp, hse_reflect_vels) &
-  !$acc create(cfl, dtnuc_e, dtnuc_X) &
-  !$acc create(dtnuc_mode, dxnuc, do_react) &
-  !$acc create(react_T_min, react_T_max, react_rho_min) &
-  !$acc create(react_rho_max, disable_shock_burning, diffuse_cutoff_density) &
-  !$acc create(do_grav, grav_source_type, do_rotation) &
-  !$acc create(rot_period, rot_period_dot, rotation_include_centrifugal) &
-  !$acc create(rotation_include_coriolis, rotation_include_domegadt, state_in_rotating_frame) &
-  !$acc create(rot_source_type, implicit_rotation_update, rot_axis) &
-  !$acc create(point_mass, point_mass_fix_solution, do_acc) &
-  !$acc create(grown_factor, track_grid_losses, const_grav) &
-  !$acc create(get_g_from_phi)
+  !$acc create(cfl, do_acc, grown_factor) &
+  !$acc create(track_grid_losses)
 
   ! End the declarations of the ParmParse parameters
 
@@ -203,7 +188,7 @@ contains
     small_pres = -1.d200;
     small_ener = -1.d200;
     do_hydro = -1;
-    do_ctu = 1;
+    do_ctu = 0;
     hybrid_hydro = 0;
     ppm_type = 1;
     ppm_trace_sources = 1;
@@ -243,36 +228,9 @@ contains
     hse_interp_temp = 0;
     hse_reflect_vels = 0;
     cfl = 0.8d0;
-    dtnuc_e = 1.d200;
-    dtnuc_X = 1.d200;
-    dtnuc_mode = 1;
-    dxnuc = 1.d200;
-    do_react = -1;
-    react_T_min = 0.0d0;
-    react_T_max = 1.d200;
-    react_rho_min = 0.0d0;
-    react_rho_max = 1.d200;
-    disable_shock_burning = 0;
-    diffuse_cutoff_density = -1.d200;
-    do_grav = -1;
-    grav_source_type = 4;
-    do_rotation = -1;
-    rot_period = -1.d200;
-    rot_period_dot = 0.0d0;
-    rotation_include_centrifugal = 1;
-    rotation_include_coriolis = 1;
-    rotation_include_domegadt = 1;
-    state_in_rotating_frame = 1;
-    rot_source_type = 4;
-    implicit_rotation_update = 1;
-    rot_axis = 3;
-    point_mass = 0.0d0;
-    point_mass_fix_solution = 0;
     do_acc = -1;
     grown_factor = 1;
     track_grid_losses = 0;
-    const_grav = 0.0d0;
-    get_g_from_phi = 0;
 
     call pp%query("difmag", difmag)
     call pp%query("small_dens", small_dens)
@@ -320,30 +278,9 @@ contains
     call pp%query("hse_interp_temp", hse_interp_temp)
     call pp%query("hse_reflect_vels", hse_reflect_vels)
     call pp%query("cfl", cfl)
-    call pp%query("dtnuc_e", dtnuc_e)
-    call pp%query("dtnuc_X", dtnuc_X)
-    call pp%query("dtnuc_mode", dtnuc_mode)
-    call pp%query("dxnuc", dxnuc)
-    call pp%query("do_react", do_react)
-    call pp%query("react_T_min", react_T_min)
-    call pp%query("react_T_max", react_T_max)
-    call pp%query("react_rho_min", react_rho_min)
-    call pp%query("react_rho_max", react_rho_max)
-    call pp%query("disable_shock_burning", disable_shock_burning)
-    call pp%query("do_grav", do_grav)
-    call pp%query("grav_source_type", grav_source_type)
-    call pp%query("do_rotation", do_rotation)
-#ifdef POINTMASS
-    call pp%query("point_mass", point_mass)
-#endif
-#ifdef POINTMASS
-    call pp%query("point_mass_fix_solution", point_mass_fix_solution)
-#endif
     call pp%query("do_acc", do_acc)
     call pp%query("grown_factor", grown_factor)
     call pp%query("track_grid_losses", track_grid_losses)
-    call pp%query("const_grav", const_grav)
-    call pp%query("get_g_from_phi", get_g_from_phi)
 
     !$acc update &
     !$acc device(difmag, small_dens, small_temp) &
@@ -359,17 +296,8 @@ contains
     !$acc device(density_reset_method, allow_negative_energy, allow_small_energy) &
     !$acc device(do_sponge, sponge_implicit, first_order_hydro) &
     !$acc device(hse_zero_vels, hse_interp_temp, hse_reflect_vels) &
-    !$acc device(cfl, dtnuc_e, dtnuc_X) &
-    !$acc device(dtnuc_mode, dxnuc, do_react) &
-    !$acc device(react_T_min, react_T_max, react_rho_min) &
-    !$acc device(react_rho_max, disable_shock_burning, diffuse_cutoff_density) &
-    !$acc device(do_grav, grav_source_type, do_rotation) &
-    !$acc device(rot_period, rot_period_dot, rotation_include_centrifugal) &
-    !$acc device(rotation_include_coriolis, rotation_include_domegadt, state_in_rotating_frame) &
-    !$acc device(rot_source_type, implicit_rotation_update, rot_axis) &
-    !$acc device(point_mass, point_mass_fix_solution, do_acc) &
-    !$acc device(grown_factor, track_grid_losses, const_grav) &
-    !$acc device(get_g_from_phi)
+    !$acc device(cfl, do_acc, grown_factor) &
+    !$acc device(track_grid_losses)
 
 
     ! now set the external BC flags
@@ -432,5 +360,62 @@ contains
     call amrex_parmparse_destroy(pp)
 
   end subroutine ca_set_castro_method_params
+
+#ifdef RADIATION
+  subroutine ca_init_radhydro_pars(fsp_type_in, do_is_in, com_in,fppt) &
+       bind(C, name="ca_init_radhydro_pars")
+
+    use rad_params_module, only : ngroups
+
+    use amrex_fort_module, only : rt => amrex_real
+    integer, intent(in) :: fsp_type_in, do_is_in, com_in
+    real(rt)        , intent(in) :: fppt
+
+    QPTOT  = QVAR+1
+    QREITOT = QVAR+2
+    QRAD = QVAR+3
+    QRADHI = qrad+ngroups-1
+  
+    QRADVAR = QVAR + 2 + ngroups
+  
+    ! update NQ -- it was already initialized in the hydro
+    NQ = QRADVAR
+
+    ! NQAUX already knows about the hydro and the non-group-dependent
+    ! rad variables, update it here
+    NQAUX = NQAUX + ngroups
+
+    if (ngroups .eq. 1) then
+       fspace_type = 1
+    else
+       fspace_type = fsp_type_in
+    end if
+    
+    if (fsp_type_in .ne. 1 .and. fsp_type_in .ne. 2) then
+       call bl_error("Unknown fspace_type", fspace_type)
+    end if
+    
+    do_inelastic_scattering = (do_is_in .ne. 0)
+    
+    if (com_in .eq. 1) then
+       comoving = .true.
+    else if (com_in .eq. 0) then
+       comoving = .false.
+    else
+       call bl_error("Wrong value for comoving", fspace_type)
+    end if
+    
+    flatten_pp_threshold = fppt
+    
+    !$acc update &
+    !$acc device(NQ,NQAUX) &
+    !$acc device(QRADVAR, QRAD, QRADHI, QPTOT, QREITOT) &
+    !$acc device(fspace_type) &
+    !$acc device(do_inelastic_scattering) &
+    !$acc device(comoving)
+    !$acc device(flatten_pp_threshold = -1.e0_rt)
+
+  end subroutine ca_init_radhydro_pars
+#endif
 
 end module meth_params_module
