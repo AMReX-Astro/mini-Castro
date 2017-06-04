9795bdd8d7393680960aaa147f7a9d85a8c787a9
Max Katz <maximilian.katz@stonybrook.edu>
2017-06-03 20:25:57 -0400

    Do not make a copy of the data in clean_state

--- Source/Castro.cpp
+++ Source/Castro.cpp
@@ -1242,11 +1242,7 @@ Castro::clean_state(MultiFab& state) {
 
     // Enforce a minimum density.
 
-    MultiFab temp_state(state.boxArray(), state.DistributionMap(), state.nComp(), state.nGrow());
-
-    MultiFab::Copy(temp_state, state, 0, 0, state.nComp(), state.nGrow());
-
-    Real frac_change = enforce_min_density(temp_state, state);
+    Real frac_change = enforce_min_density(state, state);
 
     // Ensure all species are normalized.
 
