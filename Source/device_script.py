dc1f88f96c399f827880278aa87e49746908cfe5
Max Katz <maxpkatz@gmail.com>
2018-05-05 19:18:59 -0400

    Move the __global__ implementation to the header

--- Source/device_script.py
+++ Source/device_script.py
@@ -18,8 +18,9 @@ import os
 import re
 import sys
 
+
 TEMPLATE = """
-__global__ void cuda_{}
+__global__ static void cuda_{}
 {{
 
    int blo[3];
@@ -35,10 +36,7 @@ sig_re = re.compile("(DEVICE_LAUNCHABLE)(\\()(.*)(\\))(;)", re.IGNORECASE|re.DOT
 # for finding just the variable definitions in the function signature (between the ())
 decls_re = re.compile("(.*?)(\\()(.*)(\\))", re.IGNORECASE|re.DOTALL)
 
-def doit(headers, cuda_file):
-
-    # keep track of the functions we need to CUDA-ize
-    needs_cuda = []
+def doit(headers):
 
     for hdr in headers:
         # open the header file
@@ -55,9 +53,21 @@ def doit(headers, cuda_file):
         except IOError:
             sys.exit("Cannot open output file {}".format(ofile))
 
+        # Now write out the CUDA kernels
+        hout.write("\n")
+        hout.write("#include <AMReX_BLFort.H>\n")
+        hout.write("#include <AMReX_Device.H>\n")
+        hout.write("\n")
+
+        hdrmh = hdr.strip(".H")
+
+        # Add an include guard
+        hout.write("#ifndef _cuda_" + hdrmh + "_\n")
+        hout.write("#define _cuda_" + hdrmh + "_\n\n")
+
         # Wrap the device declarations in extern "C"
         hout.write("#ifdef AMREX_USE_CUDA\n")
-        hout.write("extern \"C\" {\n")
+        hout.write("extern \"C\" {\n\n")
 
         line = hin.readline()
         while line:
@@ -77,70 +87,59 @@ def doit(headers, cuda_file):
                 m = sig_re.search(launch_sig)
                 func_sig = m.group(3)
 
-                needs_cuda.append(func_sig)
+                # First write out the device signature
+                device_sig = "__device__ void {};\n\n".format(func_sig)
+                hout.write(device_sig)
 
-                # duplicate the signature, once for device and once for global and write these out
-                orig_sig = "__device__ void {};\n\n".format(func_sig)
-                global_sig = "__global__ void cuda_{};\n\n".format(func_sig)
+                # Now write out the global signature. This involves getting
+                # rid of the data type definitions and also replacing the
+                # _first_ lo and hi with blo and bhi
+                dd = decls_re.search(func_sig)
+                vars = []
+                for n, v in enumerate(dd.group(3).split(",")):
 
-                hout.write(orig_sig)
-                hout.write(global_sig)
+                    # we will assume that our function signatures _always_ include
+                    # the name of the variable
+                    _tmp = v.split()
+                    var = _tmp[-1].replace("*","").replace("&","").strip()
+
+                    # Replace AMReX Fortran macros
+                    var = var.replace("BL_FORT_FAB_ARG_3D", "BL_FORT_FAB_VAL_3D")
+
+                    if n == 0:
+                        if var == "lo":
+                            var = "blo"
+                        else:
+                            sys.exit("ERROR: function signatures need to start with lo")
+
+                    if n == 1:
+                        if var == "hi":
+                            var = "bhi"
+                        else:
+                            sys.exit("ERROR: function signatures need hi as the second argument")
+
+                    vars.append(var)
+
+                # reassemble the function sig
+                all_vars = ", ".join(vars)
+                new_call = "{}({})".format(dd.group(1), all_vars)
+
+                hout.write(TEMPLATE.format(func_sig, new_call))
+                hout.write("\n")
 
             line = hin.readline()
 
-        # done with this header
-        hin.close()
-
         # Close out the extern "C" region
-        hout.write("}\n")
+        hout.write("\n}\n")
         hout.write("#endif\n")
+
+        # Close out the include guard
+        hout.write("\n")
+        hout.write("#endif\n")
+
+        hin.close()
         hout.close()
-
-    # we are done with all the headers now -- the only thing left is
-    # to write out the CUDA version of the file
-    with open(cuda_file, "w") as of:
-        of.write("#include <Castro.H>\n")
-        of.write("#include <Castro_F.H>\n")
-        of.write("#include <AMReX_BLFort.H>\n")
-        of.write("#include <AMReX_Device.H>\n")
-        for sig in needs_cuda:
-
-            # we need to call the cuda_ version of the routine, which
-            # means getting rid of the data type definitions and also
-            # replacing the _first_ lo and hi with blo and bhi
-            dd = decls_re.search(sig)
-            vars = []
-            for n, v in enumerate(dd.group(3).split(",")):
-
-                # we will assume that our function signatures _always_ include
-                # the name of the variable
-                _tmp = v.split()
-                var = _tmp[-1].replace("*","").replace("&","").strip()
-
-                # Replace AMReX Fortran macros
-                var = var.replace("BL_FORT_FAB_ARG_3D", "BL_FORT_FAB_VAL_3D")
-
-                if n == 0:
-                    if var == "lo":
-                        var = "blo"
-                    else:
-                        sys.exit("ERROR: function signatures need to start with lo")
-
-                if n == 1:
-                    if var == "hi":
-                        var = "bhi"
-                    else:
-                        sys.exit("ERROR: function signatures need hi as the second argument")
-
-                vars.append(var)
-
-            # reassemble the function sig
-            all_vars = ", ".join(vars)
-            new_call = "{}({})".format(dd.group(1), all_vars)
-
-            of.write(TEMPLATE.format(sig, new_call))
 
 if __name__ == "__main__":
     HEADERS = ["Castro_F.H"]
-    CUDA_FILE = "cuda_interfaces.cpp"
-    doit(HEADERS, CUDA_FILE)
+    doit(HEADERS)
