50006e5d22ce58d276b574aa2331f10ca49e03cc
Michael Zingale <michael.zingale@stonybrook.edu>
2018-05-17 11:39:55 -0400

    this is close to what we want

--- Source/write_cuda_headers.py
+++ Source/write_cuda_headers.py
@@ -79,7 +79,7 @@ def find_fortran_targets(fortran_names):
 
 def doit(outdir, fortran_targets, header_files):
 
-    for h in headers_files:
+    for h in header_files:
         hdr = "/".join([h[1], h[0]])
 
         # open the header file
@@ -96,30 +96,29 @@ def doit(outdir, fortran_targets, header_files):
         except IOError:
             sys.exit("Cannot open output file {}".format(ofile))
 
-        # Now write out the CUDA kernels
-        hout.write("\n")
-        hout.write("#include <AMReX_ArrayLim.H>\n")
-        hout.write("#include <AMReX_BLFort.H>\n")
-        hout.write("#include <AMReX_Device.H>\n")
-        hout.write("\n")
 
-        hdrmh = hdr.strip(".H")
-
-        # Add an include guard
-        hout.write("#ifndef _cuda_" + hdrmh + "_\n")
-        hout.write("#define _cuda_" + hdrmh + "_\n\n")
-
-        # Wrap the device declarations in extern "C"
-        hout.write("#ifdef AMREX_USE_CUDA\n")
-        hout.write("extern \"C\" {\n\n")
+        # we'll keep track of the signatures that we need to mangle
+        signatures = []
 
         line = hin.readline()
         while line:
 
-            # if the line doesn't have DEVICE_LAUNCHABLE, then skip it.
-            # otherwise, we need to capture the function signature
-            if "DEVICE_LAUNCHABLE" in line:
+            # if the line does not start a function signature that
+            # matches one of our targets, then we just write it out
+            # unchanged.  otherwise, we need to capture the function
+            # signature
+            found = None
 
+            # strip comments
+            idx = line.find("//")
+            tline = line[:idx]
+
+            for target in fortran_targets:
+                if target in tline.lower():
+                    found = target
+                    break
+
+            if found is not None:
                 launch_sig = "" + line
                 sig_end = False
                 while not sig_end:
@@ -128,60 +127,81 @@ def doit(outdir, fortran_targets, header_files):
                     if line.strip().endswith(";"):
                         sig_end = True
 
-                # now get just the actual signature
-                m = sig_re.search(launch_sig)
-                func_sig = m.group(3)
-
-                # First write out the device signature
-                device_sig = "__device__ void {};\n\n".format(func_sig)
-                device_sig = device_sig.replace("ARLIM_VAL", "ARLIM_REP")
-                hout.write(device_sig)
-
-                # Now write out the global signature. This involves getting
-                # rid of the data type definitions and also replacing the
-                # lo and hi (which must be in the function definition) with blo and bhi.
-                dd = decls_re.search(func_sig)
-                vars = []
-
-                has_lo = False
-                has_hi = False
-
-                for n, v in enumerate(dd.group(3).split(",")):
-
-                    # we will assume that our function signatures _always_ include
-                    # the name of the variable
-                    _tmp = v.split()
-                    var = _tmp[-1].replace("*","").replace("&","").strip()
-
-                    # Replace AMReX Fortran macros
-                    var = var.replace("BL_FORT_FAB_ARG_3D", "BL_FORT_FAB_VAL_3D")
-
-                    if var == "ARLIM_VAL(lo)":
-                        var = "blo"
-                        has_lo = True
-
-                    if var == "ARLIM_VAL(hi)":
-                        var = "bhi"
-                        has_hi = True
-
-                    vars.append(var)
-
-                if not has_lo or not has_hi:
-                    sys.exit("ERROR: function signature must have variables lo and hi defined.")
-
-                # reassemble the function sig
-                all_vars = ", ".join(vars)
-                new_call = "{}({})".format(dd.group(1), all_vars)
-
-                hout.write(TEMPLATE.format(func_sig, new_call))
-                hout.write("\n")
+                signatures.append(launch_sig)
 
             else:
                 # this wasn't a device header
                 hout.write(line)
 
-
             line = hin.readline()
+
+        # we've now finished going through the header.  All the normal
+        # signatures have simply been copied over.  Now we need to
+        # make the versions of the signatures for the device
+
+        hout.write("\n")
+        hout.write("#include <AMReX_ArrayLim.H>\n")
+        hout.write("#include <AMReX_BLFort.H>\n")
+        hout.write("#include <AMReX_Device.H>\n")
+        hout.write("\n")
+
+        hdrmh = hdr.strip(".H")
+
+        # Add an include guard -- do we still need this?
+        hout.write("#ifndef _cuda_" + hdrmh + "_\n")
+        hout.write("#define _cuda_" + hdrmh + "_\n\n")
+
+        # Wrap the device declarations in extern "C"
+        hout.write("#ifdef AMREX_USE_CUDA\n")
+        hout.write("extern \"C\" {\n\n")
+
+        for func_sig in signatures:
+
+            # First write out the device signature
+            device_sig = "__device__ void {};\n\n".format(func_sig)
+            device_sig = device_sig.replace("ARLIM_VAL", "ARLIM_REP")
+            hout.write(device_sig)
+
+            # Now write out the global signature. This involves
+            # getting rid of the data type definitions and also
+            # replacing the lo and hi (which must be in the function
+            # definition) with blo and bhi.
+            dd = decls_re.search(func_sig)
+            vars = []
+
+            has_lo = False
+            has_hi = False
+
+            for n, v in enumerate(dd.group(3).split(",")):
+
+                # we will assume that our function signatures _always_ include
+                # the name of the variable
+                _tmp = v.split()
+                var = _tmp[-1].replace("*","").replace("&","").strip()
+
+                # Replace AMReX Fortran macros
+                var = var.replace("BL_FORT_FAB_ARG_3D", "BL_FORT_FAB_VAL_3D")
+
+                if var == "ARLIM_VAL(lo)":
+                    var = "blo"
+                    has_lo = True
+
+                if var == "ARLIM_VAL(hi)":
+                    var = "bhi"
+                    has_hi = True
+
+                vars.append(var)
+
+            if not has_lo or not has_hi:
+                sys.exit("ERROR: function signature must have variables lo and hi defined.")
+
+            # reassemble the function sig
+            all_vars = ", ".join(vars)
+            new_call = "{}({})".format(dd.group(1), all_vars)
+
+            hout.write(TEMPLATE.format(func_sig, new_call))
+            hout.write("\n")
+
 
         # Close out the extern "C" region
         hout.write("\n}\n")
