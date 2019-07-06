b2735afb9b7829344051ee57dc55c747d8065b0a
Max Katz <maxpkatz@gmail.com>
2019-07-05 21:03:24 -0700

    Use FArrayBox + Elixir instead of AsyncFab

--- Source/Castro_hydro.cpp
+++ Source/Castro_hydro.cpp
@@ -27,125 +27,155 @@ Castro::construct_mol_hydro_source(Real time, Real dt, int istage, int nstages)
 #ifdef _OPENMP
 #pragma omp parallel
 #endif
-  for (MFIter mfi(S_new, hydro_tile_size); mfi.isValid(); ++mfi) {
+  {
+      FArrayBox q;
+      FArrayBox qaux;
+      FArrayBox div;
+      FArrayBox flatn;
+      FArrayBox qm;
+      FArrayBox qp;
+      FArrayBox flux[3];
+      FArrayBox qe[3];
 
-      const Box& bx = mfi.tilebox();
+      for (MFIter mfi(S_new, hydro_tile_size); mfi.isValid(); ++mfi) {
 
-      const Box& qbx = amrex::grow(bx, NUM_GROW);
+          const Box& bx = mfi.tilebox();
 
-      // Convert the conservative state to the primitive variable state.
-      // This fills both q and qaux.
+          const Box& qbx = amrex::grow(bx, NUM_GROW);
 
-      AsyncFab q(qbx, NQ);
-      AsyncFab qaux(qbx, NQAUX);
+          // Convert the conservative state to the primitive variable state.
+          // This fills both q and qaux.
 
-#pragma gpu
-      ca_ctoprim(AMREX_INT_ANYD(qbx.loVect()), AMREX_INT_ANYD(qbx.hiVect()),
-                 BL_TO_FORTRAN_ANYD(Sborder[mfi]),
-                 BL_TO_FORTRAN_ANYD(q.hostFab()),
-                 BL_TO_FORTRAN_ANYD(qaux.hostFab()));
+          q.resize(qbx, NQ);
+          Elixir elix_q = q.elixir();
 
-      const Box& obx = amrex::grow(bx, 1);
-      const Box& tbx = amrex::grow(bx, 2);
-
-      AsyncFab div(obx, 1);
-
-      // Compute divergence of velocity field.
-
-#pragma gpu
-      ca_divu(AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
-              AMREX_REAL_ANYD(dx),
-              BL_TO_FORTRAN_ANYD(q.hostFab()),
-              BL_TO_FORTRAN_ANYD(div.hostFab()));
-
-      AsyncFab flatn(obx, 1);
-
-      // Compute flattening coefficient for slope calculations.
-#pragma gpu
-      ca_uflaten
-          (AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
-           BL_TO_FORTRAN_ANYD(q.hostFab()),
-           BL_TO_FORTRAN_ANYD(flatn.hostFab()));
-
-      AsyncFab qm(tbx, 3*NQ);
-      AsyncFab qp(tbx, 3*NQ);
-
-      // Do PPM reconstruction to the zone edges.
-#pragma gpu
-      ca_ppm_reconstruct
-          (AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
-           BL_TO_FORTRAN_ANYD(q.hostFab()),
-           BL_TO_FORTRAN_ANYD(flatn.hostFab()),
-           BL_TO_FORTRAN_ANYD(qm.hostFab()),
-           BL_TO_FORTRAN_ANYD(qp.hostFab()));
-
-      q.clear();
-      flatn.clear();
-
-      Array<AsyncFab, 3> flux{AsyncFab(amrex::surroundingNodes(bx, 0), NUM_STATE),
-                              AsyncFab(amrex::surroundingNodes(bx, 1), NUM_STATE),
-                              AsyncFab(amrex::surroundingNodes(bx, 2), NUM_STATE)};
-
-      Array<AsyncFab, 3> qe{AsyncFab(amrex::surroundingNodes(bx, 0), NGDNV),
-                            AsyncFab(amrex::surroundingNodes(bx, 1), NGDNV),
-                            AsyncFab(amrex::surroundingNodes(bx, 2), NGDNV)};
-
-      for (int idir = 0; idir < 3; ++idir) {
-
-          const Box& ebx = amrex::surroundingNodes(bx, idir);
-
-          int idir_f = idir + 1;
+          qaux.resize(qbx, NQAUX);
+          Elixir elix_qaux = qaux.elixir();
 
 #pragma gpu
-          ca_construct_flux
-              (AMREX_INT_ANYD(ebx.loVect()), AMREX_INT_ANYD(ebx.hiVect()),
-               AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
+          ca_ctoprim(AMREX_INT_ANYD(qbx.loVect()), AMREX_INT_ANYD(qbx.hiVect()),
+                     BL_TO_FORTRAN_ANYD(Sborder[mfi]),
+                     BL_TO_FORTRAN_ANYD(q),
+                     BL_TO_FORTRAN_ANYD(qaux));
+
+          const Box& obx = amrex::grow(bx, 1);
+          const Box& tbx = amrex::grow(bx, 2);
+
+          div.resize(obx, 1);
+          Elixir elix_div = div.elixir();
+
+          // Compute divergence of velocity field.
+
+#pragma gpu
+          ca_divu(AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
+                  AMREX_REAL_ANYD(dx),
+                  BL_TO_FORTRAN_ANYD(q),
+                  BL_TO_FORTRAN_ANYD(div));
+
+          flatn.resize(obx, 1);
+          Elixir elix_flatn = flatn.elixir();
+
+          // Compute flattening coefficient for slope calculations.
+#pragma gpu
+          ca_uflaten
+              (AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
+               BL_TO_FORTRAN_ANYD(q),
+               BL_TO_FORTRAN_ANYD(flatn));
+
+          qm.resize(tbx, 3*NQ);
+          Elixir elix_qm = qm.elixir();
+
+          qp.resize(tbx, 3*NQ);
+          Elixir elix_qp = qp.elixir();
+
+          // Do PPM reconstruction to the zone edges.
+#pragma gpu
+          ca_ppm_reconstruct
+              (AMREX_INT_ANYD(obx.loVect()), AMREX_INT_ANYD(obx.hiVect()),
+               BL_TO_FORTRAN_ANYD(q),
+               BL_TO_FORTRAN_ANYD(flatn),
+               BL_TO_FORTRAN_ANYD(qm),
+               BL_TO_FORTRAN_ANYD(qp));
+
+          q.clear();
+          flatn.clear();
+
+          flux[0].resize(amrex::surroundingNodes(bx, 0), NUM_STATE);
+          Elixir elix_flux_x = flux[0].elixir();
+
+          flux[1].resize(amrex::surroundingNodes(bx, 1), NUM_STATE);
+          Elixir elix_flux_y = flux[1].elixir();
+
+          flux[2].resize(amrex::surroundingNodes(bx, 2), NUM_STATE);
+          Elixir elix_flux_z = flux[2].elixir();
+
+          qe[0].resize(amrex::surroundingNodes(bx, 0), NGDNV);
+          Elixir elix_qe_x = qe[0].elixir();
+
+          qe[1].resize(amrex::surroundingNodes(bx, 1), NGDNV);
+          Elixir elix_qe_y = qe[1].elixir();
+
+          qe[2].resize(amrex::surroundingNodes(bx, 2), NGDNV);
+          Elixir elix_qe_z = qe[2].elixir();
+
+          for (int idir = 0; idir < 3; ++idir) {
+
+              const Box& ebx = amrex::surroundingNodes(bx, idir);
+
+              int idir_f = idir + 1;
+
+#pragma gpu
+              ca_construct_flux
+                  (AMREX_INT_ANYD(ebx.loVect()), AMREX_INT_ANYD(ebx.hiVect()),
+                   AMREX_INT_ANYD(domain_lo), AMREX_INT_ANYD(domain_hi),
+                   AMREX_REAL_ANYD(dx), dt,
+                   idir_f,
+                   BL_TO_FORTRAN_ANYD(Sborder[mfi]),
+                   BL_TO_FORTRAN_ANYD(div),
+                   BL_TO_FORTRAN_ANYD(qaux),
+                   BL_TO_FORTRAN_ANYD(qm),
+                   BL_TO_FORTRAN_ANYD(qp),
+                   BL_TO_FORTRAN_ANYD(qe[idir]),
+                   BL_TO_FORTRAN_ANYD(flux[idir]),
+                   BL_TO_FORTRAN_ANYD(area[idir][mfi]));
+
+              Array4<Real> const flux_fab = (flux[idir]).array();
+              Array4<Real> fluxes_fab = (*fluxes[idir]).array(mfi);
+              const int numcomp = NUM_STATE;
+              const Real scale = b_mol[istage];
+
+              AMREX_HOST_DEVICE_FOR_4D(ebx, numcomp, i, j, k, n,
+              {
+                  fluxes_fab(i,j,k,n) += scale * flux_fab(i,j,k,n);
+              });
+
+          }
+
+          div.clear();
+          qaux.clear();
+          qm.clear();
+          qp.clear();
+
+#pragma gpu
+          ca_construct_hydro_update
+              (AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
                AMREX_REAL_ANYD(dx), dt,
-               idir_f,
-               BL_TO_FORTRAN_ANYD(Sborder[mfi]),
-               BL_TO_FORTRAN_ANYD(div.hostFab()),
-               BL_TO_FORTRAN_ANYD(qaux.hostFab()),
-               BL_TO_FORTRAN_ANYD(qm.hostFab()),
-               BL_TO_FORTRAN_ANYD(qp.hostFab()),
-               BL_TO_FORTRAN_ANYD(qe[idir].hostFab()),
-               BL_TO_FORTRAN_ANYD(flux[idir].hostFab()),
-               BL_TO_FORTRAN_ANYD(area[idir][mfi]));
+               b_mol[istage],
+               BL_TO_FORTRAN_ANYD(qe[0]),
+               BL_TO_FORTRAN_ANYD(qe[1]),
+               BL_TO_FORTRAN_ANYD(qe[2]),
+               BL_TO_FORTRAN_ANYD(flux[0]),
+               BL_TO_FORTRAN_ANYD(flux[1]),
+               BL_TO_FORTRAN_ANYD(flux[2]),
+               BL_TO_FORTRAN_ANYD(area[0][mfi]),
+               BL_TO_FORTRAN_ANYD(area[1][mfi]),
+               BL_TO_FORTRAN_ANYD(area[2][mfi]),
+               BL_TO_FORTRAN_ANYD(volume[mfi]),
+               BL_TO_FORTRAN_ANYD(hydro_source[mfi]));
 
-          Array4<Real> const flux_fab = (flux[idir]).fabPtr()->array();
-          Array4<Real> fluxes_fab = (*fluxes[idir]).array(mfi);
-          const int numcomp = NUM_STATE;
-          const Real scale = b_mol[istage];
+      } // MFIter loop
 
-          AMREX_HOST_DEVICE_FOR_4D(ebx, numcomp, i, j, k, n,
-          {
-              fluxes_fab(i,j,k,n) += scale * flux_fab(i,j,k,n);
-          });
-
-      }
-
-      div.clear();
-      qaux.clear();
-      qm.clear();
-      qp.clear();
-
-#pragma gpu
-      ca_construct_hydro_update
-          (AMREX_INT_ANYD(bx.loVect()), AMREX_INT_ANYD(bx.hiVect()),
-           AMREX_REAL_ANYD(dx), dt,
-           b_mol[istage],
-           BL_TO_FORTRAN_ANYD(qe[0].hostFab()),
-           BL_TO_FORTRAN_ANYD(qe[1].hostFab()),
-           BL_TO_FORTRAN_ANYD(qe[2].hostFab()),
-           BL_TO_FORTRAN_ANYD(flux[0].hostFab()),
-           BL_TO_FORTRAN_ANYD(flux[1].hostFab()),
-           BL_TO_FORTRAN_ANYD(flux[2].hostFab()),
-           BL_TO_FORTRAN_ANYD(area[0][mfi]),
-           BL_TO_FORTRAN_ANYD(area[1][mfi]),
-           BL_TO_FORTRAN_ANYD(area[2][mfi]),
-           BL_TO_FORTRAN_ANYD(volume[mfi]),
-           BL_TO_FORTRAN_ANYD(hydro_source[mfi]));
-
-  } // MFIter loop
+  } // OpenMP loop
 
   BL_PROFILE_VAR_STOP(CA_HYDRO);
 
