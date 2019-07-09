139d5f5ef47fe0693227ec549aa33fed64df20af
Max Katz <maxpkatz@gmail.com>
2019-07-09 07:10:59 -0700

    Consolidate Castro_mol.F90 and advection_util.F90

--- Source/Castro_hydro_nd.F90
+++ Source/Castro_hydro_nd.F90
@@ -1,10 +1,60 @@
-module advection_util_module
+module hydro_module
 
   use amrex_fort_module, only: rt => amrex_real
 
   implicit none
 
 contains
+
+  subroutine ca_construct_flux(lo, hi, domlo, domhi, dx, dt, idir, &
+                               uin, uin_lo, uin_hi, &
+                               div, div_lo, div_hi, &
+                               qaux, qa_lo, qa_hi, &
+                               qm, qm_lo, qm_hi, &
+                               qp, qp_lo, qp_hi, &
+                               qint, qe_lo, qe_hi, &
+                               flux, f_lo, f_hi, &
+                               area, a_lo, a_hi) &
+                               bind(c,name='ca_construct_flux')
+
+    use amrex_fort_module, only: rt => amrex_real
+    use meth_params_module, only: NVAR, NGDNV, NQAUX, NQ
+    use riemann_module, only: cmpflx
+
+    implicit none
+
+    integer,  intent(in   ) :: lo(3), hi(3)
+    integer,  intent(in   ), value :: idir
+    integer,  intent(in   ) :: domlo(3), domhi(3)
+    integer,  intent(in   ) :: uin_lo(3), uin_hi(3)
+    integer,  intent(in   ) :: div_lo(3), div_hi(3)
+    integer,  intent(in   ) :: qa_lo(3), qa_hi(3)
+    integer,  intent(in   ) :: qm_lo(3), qm_hi(3)
+    integer,  intent(in   ) :: qp_lo(3), qp_hi(3)
+    integer,  intent(in   ) :: qe_lo(3), qe_hi(3)
+    integer,  intent(in   ) :: f_lo(3), f_hi(3)
+    integer,  intent(in   ) :: a_lo(3), a_hi(3)
+
+    real(rt), intent(in   ) :: uin(uin_lo(1):uin_hi(1), uin_lo(2):uin_hi(2), uin_lo(3):uin_hi(3), NVAR)
+    real(rt), intent(in   ) :: div(div_lo(1):div_hi(1), div_lo(2):div_hi(2), div_lo(3):div_hi(3))
+    real(rt), intent(in   ) :: qaux(qa_lo(1):qa_hi(1), qa_lo(2):qa_hi(2), qa_lo(3):qa_hi(3), NQAUX)
+    real(rt), intent(inout) :: qm(qm_lo(1):qm_hi(1),qm_lo(2):qm_hi(2),qm_lo(3):qm_hi(3),NQ,3)
+    real(rt), intent(inout) :: qp(qp_lo(1):qp_hi(1),qp_lo(2):qp_hi(2),qp_lo(3):qp_hi(3),NQ,3)
+    real(rt), intent(inout) :: qint(qe_lo(1):qe_hi(1), qe_lo(2):qe_hi(2), qe_lo(3):qe_hi(3), NGDNV)
+    real(rt), intent(inout) :: flux(f_lo(1):f_hi(1), f_lo(2):f_hi(2), f_lo(3):f_hi(3), NVAR)
+    real(rt), intent(in   ) :: area(a_lo(1):a_hi(1), a_lo(2):a_hi(2), a_lo(3):a_hi(3))
+    real(rt), intent(in   ) :: dx(3)
+    real(rt), intent(in   ), value :: dt
+
+    !$gpu
+
+    call cmpflx(lo, hi, domlo, domhi, idir, qm, qm_lo, qm_hi, qp, qp_lo, qp_hi, &
+                qint, qe_lo, qe_hi, flux, f_lo, f_hi, qaux, qa_lo, qa_hi)
+    call apply_av(lo, hi, idir, dx, div, div_lo, div_hi, uin, uin_lo, uin_hi, flux, f_lo, f_hi)
+    call normalize_species_fluxes(lo, hi, flux, f_lo, f_hi)
+    call scale_flux(lo, hi, flux, f_lo, f_hi, area, a_lo, a_hi, dt)
+
+  end subroutine ca_construct_flux
 
   subroutine ca_enforce_minimum_density(uin,uin_lo,uin_hi, &
                                         uout,uout_lo,uout_hi, &
@@ -43,14 +93,7 @@ contains
        do j = lo(2), hi(2)
           do i = lo(1), hi(1)
 
-             if (uout(i,j,k,URHO) .eq. ZERO) then
-
-#ifndef AMREX_USE_CUDA
-                print *,'DENSITY EXACTLY ZERO AT CELL ',i,j,k
-                print *,'  in grid ',lo(1),lo(2),lo(3),hi(1),hi(2),hi(3)
-                call bl_error("Error:: advection_util_nd.F90 :: ca_enforce_minimum_density")
-#endif
-             else if (uout(i,j,k,URHO) < small_dens) then
+             if (uout(i,j,k,URHO) < small_dens) then
 
                 old_state = uin(i,j,k,:)
 
@@ -224,17 +267,6 @@ contains
              ! method-of-lines constraint
              courtmp = courx + coury + courz
 
-#ifndef AMREX_USE_CUDA
-             ! note: it might not be 1 for all RK integrators
-             if (courtmp > ONE) then
-                print *,'   '
-                call bl_warning("Warning:: advection_util_nd.F90 :: CFL violation in compute_cfl")
-                print *,'>>> ... at cell (i,j,k)   : ', i, j, k
-                print *,'>>> ... u,v,w, c            ', q(i,j,k,QU), q(i,j,k,QV), q(i,j,k,QW), qaux(i,j,k,QC)
-                print *,'>>> ... density             ', q(i,j,k,QRHO)
-             endif
-#endif
-
              call amrex_max(courno, courtmp)
 
           enddo
@@ -288,20 +320,6 @@ contains
     do k = lo(3), hi(3)
        do j = lo(2), hi(2)
           do i = lo(1), hi(1)
-
-#ifndef AMREX_USE_CUDA
-             if (uin(i,j,k,URHO) .le. ZERO) then
-                print *,'   '
-                print *,'>>> Error: advection_util_nd.F90::ctoprim ',i, j, k
-                print *,'>>> ... negative density ', uin(i,j,k,URHO)
-                call bl_error("Error:: advection_util_nd.F90 :: ctoprim")
-             else if (uin(i,j,k,URHO) .lt. small_dens) then
-                print *,'   '
-                print *,'>>> Error: advection_util_nd.F90::ctoprim ',i, j, k
-                print *,'>>> ... small density ', uin(i,j,k,URHO)
-                call bl_error("Error:: advection_util_nd.F90 :: ctoprim")
-             endif
-#endif
 
              q(i,j,k,QRHO) = uin(i,j,k,URHO)
              rhoinv = ONE/q(i,j,k,QRHO)
@@ -427,9 +445,7 @@ contains
 
   end subroutine normalize_species_fluxes
 
-! :::
-! ::: ------------------------------------------------------------------
-! :::
+
 
   subroutine ca_divu(lo, hi, dx, q, q_lo, q_hi, div, d_lo, d_hi) bind(c,name='ca_divu')
 
@@ -676,5 +692,5 @@ contains
     enddo
 
   end subroutine scale_flux
-
-end module advection_util_module
+  
+end module hydro_module
